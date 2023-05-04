88e544eedf343158048f1eff70cb3d23d2484771
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2023-05-04 13:52:15 -0300

    server: initial commit

---
+++ apps/server/src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { version } from '../package.json';
+
+@Injectable()
+export class AppService {
+  getAppVersion(): string {
+    return version;
+  }
+}
