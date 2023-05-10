f66efcaecf3ef7c6ec2efd251e16d3a5c4b55af8
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2023-05-10 14:42:44 -0300

    feat: add timestamps to user model

--- apps/server/priv/repo/migrations/20230510165005_create_users.exs
+++ apps/server/priv/repo/migrations/20230510165005_create_users.exs
@@ -2,12 +2,14 @@ defmodule Lunar.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :first_name, :string, null: false
       add :last_name, :string, null: false
       add :email, :string, null: false
+      add :is_email_verified, :boolean, null: false, default: false
 
-      timestamps()
+      timestamps(default: fragment("now()"))
     end
 
     create index(:users, [:email], unique: true)
