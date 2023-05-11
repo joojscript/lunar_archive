df35b24c45357e34d45f8f8c1bd3d388a873a192
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2023-05-11 13:07:59 -0300

    bug(test_helper.exs): fix test

--- apps/server/test/test_helper.exs
+++ apps/server/test/test_helper.exs
@@ -3,6 +3,7 @@ ExUnit.start()
 Lunar.Repo.start_link()
 
 # Remove tmp database:
+tmp_database_path = "/tmp/lunar_test.sqlite3"
 if File.exists?(tmp_database_path) do
   File.rm!(tmp_database_path)
 end
